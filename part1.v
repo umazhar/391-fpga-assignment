835bb7cb3f56c35a44b6ae99b52c86cabd423325
Zephaniah Ko <40482490+Zephko@users.noreply.github.com>
2022-05-24 15:48:02 -0700

    add noise generator, commented out part 2 avg code

--- part1.v
+++ part1.v
@@ -17,18 +17,59 @@ module part1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK,
 	wire [23:0] readdata_left, readdata_right;
 	wire [23:0] writedata_left, writedata_right, writedata_left_buff, writedata_right_buff;
 	wire reset = ~KEY[0];
+	reg [23:0] lbuff0, lbuff1, lbuff2, lbuff3, lbuff4, lbuff5, lbuff6, lbuff7;
+	reg [23:0] rbuff0, rbuff1, rbuff2, rbuff3, rbuff4, rbuff5, rbuff6, rbuff7;
+	reg [23:0] sum_left;
+	reg [23:0] sum_right;
+	wire [23:0] noise;
+	wire enable = 1'b1;
 
 	/////////////////////////////////
 	// Your code goes here 
 	/////////////////////////////////
 	
-	assign writedata_left = write_ready ? readdata_left : writedata_left;
-	assign writedata_right = write_ready ? readdata_right : writedata_right;
+	assign writedata_left = write_ready ? sum_left : writedata_left;
+	assign writedata_right = write_ready ? sum_right : writedata_right;
 	assign read = read_ready;
 	assign write = write_ready;
 	
+
+always @ (posedge CLOCK_50)
+	begin
+		if(read_ready == 1'b1)
+			begin
+//				lbuff0 <= readdata_left >> 3;
+//				lbuff1 <= lbuff0;
+//				lbuff2 <= lbuff1;
+//				lbuff3 <= lbuff2;
+//				lbuff4 <= lbuff3;
+//				lbuff5 <= lbuff4;
+//				lbuff6 <= lbuff5;
+//				lbuff7 <= lbuff6;
+//				sum_left <= lbuff0 + lbuff1 + lbuff2 + lbuff3 + lbuff4 + lbuff5 + lbuff6 + lbuff7;	
+				sum_left <= readdata_left+noise;		
+			end
+	end
 	
+always @ (posedge CLOCK_50)
+	begin
+		if(read_ready == 1'b1)
+			begin
+//				rbuff0 <= readdata_right >> 3;
+//				rbuff1 <= rbuff0;
+//				rbuff2 <= rbuff1;
+//				rbuff3 <= rbuff2;
+//				rbuff4 <= rbuff3;
+//				rbuff5 <= rbuff4;
+//				rbuff6 <= rbuff5;
+//				rbuff7 <= rbuff6;
+//				sum_right <= rbuff0 + rbuff1 + rbuff2 + rbuff3 + rbuff4 + rbuff5 + rbuff6 + rbuff7;		
+				sum_right <= readdata_right+noise;
+			end
+	end
 	
+noise_generator ng(CLOCK50, enable, noise);
+
 	
 	
 /////////////////////////////////////////////////////////////////////////////////
@@ -87,12 +128,14 @@ module part1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK,
 
 endmodule
 
-//	module noise_generator(clk, enable, Q);
-//	input clk, enable;
-//    	output [23:0] Q;
-//	reg [2:0] counter;
-//	always@(posedge clk)
-//		if (enable)	
-//			counter = counter + 1'b1;
-//	assign Q = {{10{counter[2]}}, counter, 11'd0};
-//	end module;
+	
+module noise_generator (clk, enable, Q);
+input clk, enable;
+output [23:0] Q;
+reg [2:0] counter;
+always@ (posedge clk)
+	if (enable)
+		counter = counter + 1'b1;
+assign Q = {{10{counter[2]}}, counter, 11'd0};
+endmodule
+	
