b8a93f007afd5dcdc93c5e5bc3664f09a4c20e9a
Umair Mazhar <46656791+umazhar@users.noreply.github.com>
2022-05-24 15:32:13 -0700

    Update part1.v

--- part1.v
+++ part1.v
@@ -86,3 +86,13 @@ module part1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK,
 	);
 
 endmodule
+
+//	module noise_generator(clk, enable, Q);
+//	input clk, enable;
+//    	output [23:0] Q;
+//	reg [2:0] counter;
+//	always@(posedge clk)
+//		if (enable)	
+//			counter = counter + 1'b1;
+//	assign Q = {{10{counter[2]}}, counter, 11'd0};
+//	end module;
