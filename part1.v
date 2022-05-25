612dbcdb7413fa579afc892fb2e08f57509ba497
Umair Mazhar <46656791+umazhar@users.noreply.github.com>
2022-05-25 12:57:25 -0700

    Update part1.v

--- part1.v
+++ part1.v
@@ -15,63 +15,24 @@ module part1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK,
 	// Local wires.
 	wire read_ready, write_ready, read, write;
 	wire [23:0] readdata_left, readdata_right;
-	wire [23:0] writedata_left, writedata_right, writedata_left_buff, writedata_right_buff;
+	wire [23:0] writedata_left, writedata_right;
 	wire reset = ~KEY[0];
-	reg [23:0] lbuff0, lbuff1, lbuff2, lbuff3, lbuff4, lbuff5, lbuff6, lbuff7;
-	reg [23:0] rbuff0, rbuff1, rbuff2, rbuff3, rbuff4, rbuff5, rbuff6, rbuff7;
-	reg [23:0] sum_left;
-	reg [23:0] sum_right;
-	wire [23:0] noise;
 	wire enable = 1'b1;
 
 	/////////////////////////////////
 	// Your code goes here 
 	/////////////////////////////////
+
+	wire [23:0] filter_out_left, filter_out_right;
 	
-	assign writedata_left = write_ready ? sum_left : writedata_left;
-	assign writedata_right = write_ready ? sum_right : writedata_right;
+	assign writedata_left = write_ready ? filter_out_left : writedata_left;
+	assign writedata_right = write_ready ? filter_out_right : writedata_right;
 	assign read = read_ready;
 	assign write = write_ready;
 	
+	filter FILTER_LEFT(CLOCK50, readdata_left, filter_out_left, read_ready);
+	filter FILTER_RIGHT(CLOCK50, readdata_right, filter_out_right, read_ready);
 
-always @ (posedge CLOCK_50)
-	begin
-		if(read_ready == 1'b1)
-			begin
-//				lbuff0 <= readdata_left >> 3;
-//				lbuff1 <= lbuff0;
-//				lbuff2 <= lbuff1;
-//				lbuff3 <= lbuff2;
-//				lbuff4 <= lbuff3;
-//				lbuff5 <= lbuff4;
-//				lbuff6 <= lbuff5;
-//				lbuff7 <= lbuff6;
-//				sum_left <= lbuff0 + lbuff1 + lbuff2 + lbuff3 + lbuff4 + lbuff5 + lbuff6 + lbuff7;	
-				sum_left <= readdata_left+noise;		
-			end
-	end
-	
-always @ (posedge CLOCK_50)
-	begin
-		if(read_ready == 1'b1)
-			begin
-//				rbuff0 <= readdata_right >> 3;
-//				rbuff1 <= rbuff0;
-//				rbuff2 <= rbuff1;
-//				rbuff3 <= rbuff2;
-//				rbuff4 <= rbuff3;
-//				rbuff5 <= rbuff4;
-//				rbuff6 <= rbuff5;
-//				rbuff7 <= rbuff6;
-//				sum_right <= rbuff0 + rbuff1 + rbuff2 + rbuff3 + rbuff4 + rbuff5 + rbuff6 + rbuff7;		
-				sum_right <= readdata_right+noise;
-			end
-	end
-	
-noise_generator ng(CLOCK50, enable, noise);
-
-	
-	
 /////////////////////////////////////////////////////////////////////////////////
 // Audio CODEC interface. 
 //
@@ -128,14 +89,49 @@ noise_generator ng(CLOCK50, enable, noise);
 
 endmodule
 
-	
 module noise_generator (clk, enable, Q);
-input clk, enable;
-output [23:0] Q;
-reg [2:0] counter;
-always@ (posedge clk)
-	if (enable)
-		counter = counter + 1'b1;
-assign Q = {{10{counter[2]}}, counter, 11'd0};
+	input clk, enable;
+	output [23:0] Q;
+	reg [2:0] counter;
+	always@ (posedge clk)
+		if (enable)
+			counter = counter + 1'b1;
+	assign Q = {{10{counter[2]}}, counter, 11'd0};
 endmodule
-	
+
+module filter(CLOCK_50, in, out, read_ready);
+    input CLOCK_50;
+    input [23:0] in;
+    output [23:0] out;
+    input read_ready;
+
+    //registers for 8 stage filter
+	reg [23:0] buff0, buff1, buff2, buff3, buff4, buff5, buff6, buff7;
+	reg [23:0] sum;
+	wire [23:0] noise;
+
+    wire enable = 1'b1;
+    noise_generator ng(CLOCK50, enable, noise);
+
+    always @ (posedge CLOCK_50)
+	begin
+		if(read_ready == 1'b1)
+			begin
+				buff0 <= in >> 3;
+				buff1 <= buff0;
+				buff2 <= buff1;
+			    buff3 <= buff2;
+				buff4 <= buff3;
+				buff5 <= buff4;
+				buff6 <= buff5;
+				buff7 <= buff6;
+				
+				//sum <= in;		
+			end
+	end
+
+	always @(*) begin
+		sum = buff0 + buff1 + buff2 + buff3 + buff4 + buff5 + buff6 + buff7;	
+	end
+	assign out = sum;
+endmodule
